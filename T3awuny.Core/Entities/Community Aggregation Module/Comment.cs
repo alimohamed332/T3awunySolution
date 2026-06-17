d0d309fbcb175f931c3b4d7a01b572019b30feac
Mohamed Elwani <mohamedelwani5284@gmail.com>
2026-06-17 17:06:31 +0300

    Add Community Module Entities

---
+++ T3awuny.Core/Entities/Community Aggregation Module/Comment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T3awuny.Core.Entities.UserModule;
+
+namespace T3awuny.Core.Entities.Community_Aggregation_Module
+{
+	public class Comment : BaseEntity
+	{
+		public string Id { get; set; } = Guid.NewGuid().ToString();
+		public string Description { get; set; } = default!;
+		public DateTime CreatedAt { get; private set; } = DateTime.Now;
+
+		//Forigen Keys
+		public string PostId { get; set; } = default!;
+		[ForeignKey(nameof(Comment.Id))]
+		public string? ParentCommentId {  get; set; }
+		[ForeignKey(nameof(ApplicationUser.Id))]
+		public string UserId { get; set; } = default!;
+	}
+}
---
+++ T3awuny.Core/Entities/Community Aggregation Module/Post.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using T3awuny.Core.Entities.UserModule;
+
+namespace T3awuny.Core.Entities.Community_Aggregation_Module
+{
+	public class Post : BaseEntity
+	{
+		public string Id { get; set; } = Guid.NewGuid().ToString();
+		public DateTime CreatedAt { get; private set; } = DateTime.Now;
+		public string Title { get; set; } =String.Empty;
+		public string Description { get; set; } = default!;
+		//Navigation Properties
+		public Category? Category { get; set; }
+		public ApplicationUser? Publisher { get; set; }
+		public IReadOnlyList<PostFile>? Files { get; set; }
+		//Forigen Keys
+		public int CategoryId { get; set; }
+		[ForeignKey(nameof(ApplicationUser.Id))]
+		public string UserId { get; set; } = default!;
+		//Helper Properties
+		[NotMapped]
+		public TimeSpan CreatedSince => DateTime.Now - CreatedAt;
+		
+
+	}
+}
